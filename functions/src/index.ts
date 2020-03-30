ccab36de207db2491b037d44572eaaac8fddde50
Antoine Giret <ant.giret@gmail.com>
2020-03-30 10:16:28 +0200

    chore: add firebase

---
+++ functions/src/index.ts
@@ -0,0 +1,12 @@
+import { auth } from 'firebase-functions'
+import { firestore, initializeApp } from 'firebase-admin'
+
+initializeApp()
+
+// eslint-disable-next-line import/prefer-default-export
+export const createUser = auth.user().onCreate(async ({ uid, email, displayName }) => {
+  await firestore()
+    .collection('users')
+    .doc(uid)
+    .set({ email, displayName: displayName || '' })
+})
---
+++ src/services/firebase.ts
@@ -0,0 +1,47 @@
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_APP_ID,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_DATABASE_URL,
+  FIREBASE_MEASUREMENT_ID,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+} from 'react-native-dotenv'
+import { app as firebaseApp, auth, initializeApp } from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
+
+class FirebaseService {
+  private static instance: FirebaseService
+
+  auth: auth.Auth
+
+  constructor(private readonly app: firebaseApp.App) {
+    this.auth = app.auth()
+
+    console.log('[FirebaseService] initialized')
+  }
+
+  static getInstance() {
+    if (!FirebaseService.instance) {
+      FirebaseService.instance = new FirebaseService(
+        initializeApp({
+          apiKey: FIREBASE_API_KEY,
+          authDomain: FIREBASE_AUTH_DOMAIN,
+          databaseURL: FIREBASE_DATABASE_URL,
+          projectId: FIREBASE_PROJECT_ID,
+          storageBucket: FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+          appId: FIREBASE_APP_ID,
+          measurementId: FIREBASE_MEASUREMENT_ID,
+        }),
+      )
+    }
+
+    return FirebaseService.instance
+  }
+}
+
+export default FirebaseService
